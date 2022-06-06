7955f247ca3551f13eb1a4cc7104aeb004699edd
RafaAlbano <rafaelaalbano70@gmail.com>
2022-06-06 11:15:52 -0300

    060622

--- src/views/JogadoresView.vue
+++ src/views/JogadoresView.vue
@@ -1,3 +1,43 @@
 <template>
-  <main>Jogadores</main>
+  <div class="container">
+    <div class="title">
+      <h2>Gerenciamento de Jogadores</h2>
+    </div>
+    <div class="form_input">
+      <input type="text" />
+      <button>Salvar</button>
+    </div>
+    <div class="list_times">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+            <th>Times</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Time 1</td>
+            <td>???</td>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Time 2</td>
+            <td>???</td>
+            <td>???</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Time 3</td>
+            <td>???</td>
+            <td>???</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
